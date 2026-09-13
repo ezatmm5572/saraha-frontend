2045139e3747642eeabe04235d090e1f549c97ed
ezatmm5572 <almsryabwzt@gmail.com>
2026-09-13 06:29:05 +0300

    Add Google social login

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "./assets/main.css"
 
-createApp(App).use(router).mount("#app")
+import vue3GoogleLogin from "vue3-google-login"
+
+const app = createApp(App)
+
+app.use(router)
+
+app.use(vue3GoogleLogin, {
+    clientId: "882940930891-civns95juvot511h26l0bv894o5ra5h9.apps.googleusercontent.com"
+})
+
+app.mount("#app")
--- src/services/backend.js
+++ src/services/backend.js
@@ -2,9 +2,18 @@ import api from "./api"
 
 export const backend = {
     signUp: (payload) => api.post("/user/signUp", payload),
+
     signIn: (payload) => api.post("/user/signIn", payload),
+
+    signUpWithGoogle: (payload) =>
+        api.post("/user/signUp/gmail", payload),
+
     getProfile: () => api.get("/user/profile"),
+
     getMyMessages: () => api.get("/user/messages"),
+
     getAIInsights: () => api.get("/ai/message-insights"),
-    sendMessage: (userID, payload) => api.post(`/messages/message/${userID}`, payload)
+
+    sendMessage: (userID, payload) =>
+        api.post(`/messages/message/${userID}`, payload)
 }
